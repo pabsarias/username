7f820697e88f3196d55ba0c148853e2fb8b1e119
Pablo Arias <parias@chapman.edu>
2018-11-05 14:22:47 -0800

    adds username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 echo "Enter a username: "
 read NAME
-while echo $NAME | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo $NAME | egrep -v "^[a-z][_0-9a-z]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username: "
